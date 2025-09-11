79d17589ce1256590bda29524a90ebbe22cd4534
Piotr Kowalski <piotr.98@icloud.com>
2025-09-11 20:57:00 +0200

    Create config for sender

--- client/sender.go
+++ client/sender.go
@@ -2,6 +2,7 @@ package client
 
 import (
 	"net/smtp"
+	"relaygp/config"
 )
 
 type MessageOpts struct {
@@ -10,10 +11,18 @@ type MessageOpts struct {
 }
 
 func SendMessage(opts MessageOpts) error {
+	config := config.GetCurrentConfig().SenderConfig
+
+	var auth smtp.Auth
+
+	if config.SmtpUser != "" && config.SmtpPassword != "" {
+		auth = smtp.PlainAuth("", config.SmtpUser, config.SmtpPassword, config.SmtpAddress)
+	}
+
 	return smtp.SendMail(
-		"localhost:2525",
-		nil,
-		"test@localhost",
+		config.SmtpAddress,
+		auth,
+		config.SenderName,
 		[]string{opts.To},
 		opts.Data,
 	)
